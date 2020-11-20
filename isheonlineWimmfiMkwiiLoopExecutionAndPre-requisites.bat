df88e60c02b5c96be29f464456ba4d1fdc0c9a17
PepeElGrasas <49036522+piooop@users.noreply.github.com>
2020-11-20 23:31:29 +0100

    Update isheonlineWimmfiMkwiiLoopExecutionAndPre-requisites.bat

--- isheonlineWimmfiMkwiiLoopExecutionAndPre-requisites.bat
+++ isheonlineWimmfiMkwiiLoopExecutionAndPre-requisites.bat
@@ -16,7 +16,7 @@ IF EXIST .\apseo.py (
 )
 REM comment_out: This copies the file apseo.py to python library folder if in folder
 IF NOT EXIST C:\Python27\p.mp3 (
-         wget https://downloads.khinsider.com/game-soundtracks/album/mario-kart-wii/3-32%2520Finish%2520OK%2520%2526%2520Winning%2520Results%2520%2528Race%2529.mp3           
+         wget https://vgmsite.com/soundtracks/mario-kart-wii/yenpskzutw/3-32%20Finish%20OK%20%26%20Winning%20Results%20%28Race%29.mp3           
          copy .\p.mp3 C:\Python27
          del .\p.mp3
          copy 3-32 Finish OK & Winning Results (Race).mp3 p.mp3
