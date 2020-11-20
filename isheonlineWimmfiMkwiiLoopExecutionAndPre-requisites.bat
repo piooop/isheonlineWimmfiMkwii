728476038a9176fcf7ed184ed9b07090ebde5bd3
PepeElGrasas <49036522+piooop@users.noreply.github.com>
2020-11-20 23:04:07 +0100

    Update isheonlineWimmfiMkwiiLoopExecutionAndPre-requisites.bat

--- isheonlineWimmfiMkwiiLoopExecutionAndPre-requisites.bat
+++ isheonlineWimmfiMkwiiLoopExecutionAndPre-requisites.bat
@@ -19,6 +19,7 @@ IF NOT EXIST C:\Python27\p.mp3 (
          wget -o p.mp3 https://downloads.khinsider.com/game-soundtracks/album/mario-kart-wii/3-32%2520Finish%2520OK%2520%2526%2520Winning%2520Results%2520%2528Race%2529.mp3           
          copy .\p.mp3 C:\Python27
          del .\p.mp3
+         copy 3-32 Finish OK & Winning Results (Race).mp3 p.mp3
 )
 REM comment_out: If you don't have the music file it downloads it and copies it to python folder and deletes the one downloaded
 C:\Python27\Scripts\pip.exe install wget
